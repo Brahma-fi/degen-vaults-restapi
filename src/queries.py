2b1f478fe56ac511bc8be77d90beb6434c4c93a7
pradeep-selva <pradeepselvaraju@gmail.com>
2022-04-08 17:19:58 +0530

    ft: changes for gunicorn

--- src/queries.py
+++ src/queries.py
@@ -1,4 +1,4 @@
-from database import execute_statement, row_formatter, DATABASE_NAME, HISTORIC_REWARDS_TABLE_NAME
+from src.database import execute_statement, row_formatter, DATABASE_NAME, HISTORIC_REWARDS_TABLE_NAME
 
 def get_historic_rewards_data():
     query = f'''select * from {DATABASE_NAME}.{HISTORIC_REWARDS_TABLE_NAME};'''
