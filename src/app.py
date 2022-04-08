308f7c69bcfc03cae6dea8874fa4d10768207fd0
pradeep-selva <pradeepselvaraju@gmail.com>
2022-04-08 17:36:37 +0530

    ft: healthcheck endpoint

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ from src.queries import get_historic_rewards_data
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/", methods=['GET'])
+def health_check():
+    return {'status':'OK'}
+
 @app.route("/historic_rewards", methods=['GET'])
 def get_historic_rewards():
     result = get_historic_rewards_data()
