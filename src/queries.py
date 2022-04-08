a818c63efe0118dbfc9bbb5560c33623021bfeef
pradeep-selva <pradeepselvaraju@gmail.com>
2022-04-08 16:07:39 +0530

    ft: fixes in query

--- src/queries.py
+++ src/queries.py
@@ -1,14 +1,10 @@
-import pandas as pd
 from database import execute_statement, row_formatter, DATABASE_NAME, HISTORIC_REWARDS_TABLE_NAME
 
 def get_historic_rewards_data():
     query = f'''select * from {DATABASE_NAME}.{HISTORIC_REWARDS_TABLE_NAME};'''
-    result = execute_statement(query, [])
+    response = execute_statement(query, [])
+    result = response['records']
 
-    out = [row_formatter(row) for row in result['records']]
-    out = pd.DataFrame(out)
-
-    out['timestamp'] = pd.to_datetime(out['timestamp'])
-    out.sort_values(by=['timestamp'], inplace=True)
+    out = [row_formatter(row) for row in result]
 
     return out
