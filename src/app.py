1aaf78246eaa11e5a63d38479902d2b1c043f847
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-03 19:00:30 +0530

    ft: response formattor

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask_cors import CORS
 from flask import Flask, jsonify
 from configs.database import TABLE_NAMES
 from configs.response import RESPONSE_KEYS
+from utils.formatting import Formattor
 from utils.queries import Queries
 
 app = Flask(__name__)
@@ -17,28 +18,28 @@ def get_historic_rewards():
         TABLE_NAMES.historic_rewards, 
         RESPONSE_KEYS.claimed_rewards
     )
-    return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
+    return  Formattor().formatted_response(result)
 
 @app.route("/latest_buffer", methods=['GET'])
 def get_latest_buffer():
     result = Queries().get_all_timestamp_value_data(TABLE_NAMES.buffer_values, RESPONSE_KEYS.buffer)
-    return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
+    return  Formattor().formatted_response(result)
 
 @app.route("/open_timestamps", methods=['GET'])
 def get_open_positions():
     result = Queries().get_open_positions_data()
-    return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
+    return  Formattor().formatted_response(result)
 
 
 @app.route("/share_price", methods=['GET'])
 def get_share_prices():
     result = Queries().get_all_timestamp_value_data(TABLE_NAMES.share_price_db, RESPONSE_KEYS.price)
-    return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
+    return  Formattor().formatted_response(result)
 
 @app.route("/apr", methods=['GET'])
 def get_apr_values():
     result = Queries().get_latest_timestamp_value_data(TABLE_NAMES.share_price_db, RESPONSE_KEYS.apr)
-    return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
+    return  Formattor().formatted_response(result)
 
 
 
