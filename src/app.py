b267befc7e3188ae5df5a3ea93a65749119ac361
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-03 18:12:19 +0530

    ft: /latest_buffer endpoint

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask_cors import CORS
 from flask import Flask, jsonify
-from src.queries import get_historic_rewards_data
+from queries import get_historic_rewards_data, get_latest_buffer_value
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +13,11 @@ def health_check():
 def get_historic_rewards():
     result = get_historic_rewards_data()
     return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
+
+@app.route("/latest_buffer", methods=['GET'])
+def get_latest_buffer():
+    result = get_latest_buffer_value()
+    return  (jsonify(data = result[-1] if len(result) > 0 else {}), 200, {"ContentType": 'application/json'})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
