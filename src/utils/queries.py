318e71060f7aa83f9e678cc01cc74fde02d0fab9
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-03 18:49:09 +0530

    ft: made queries more generic

--- src/utils/queries.py
+++ src/utils/queries.py
@@ -1,22 +1,20 @@
 from configs.database import DATABASE_NAME, TABLE_NAMES
-from configs.response import RESPONSE_KEYS
-from utils.database import formatted_latest_timestamp_value_entry, timestamp_key_row_formatter,execute_statement
+from utils.database import execute_statement
+from utils.formatting import Formattor
 
-def get_historic_rewards_data():
-    query = f'''select * from {DATABASE_NAME}.{TABLE_NAMES.historic_rewards};'''
+def get_all_timestamp_value_data(table_name, key_name):
+    query = f'''select * from {DATABASE_NAME}.{table_name};'''
     response = execute_statement(query, [])
     result = response['records']
 
-    out = [timestamp_key_row_formatter(row, RESPONSE_KEYS.claimed_rewards) for row in result]
+    return Formattor().formatted_all_timestamp_value_entries(result, key_name)
 
-    return out
-
-def get_latest_buffer_value():
-    query = f'''select * from {DATABASE_NAME}.{TABLE_NAMES.buffer_values};'''
+def get_latest_timestamp_value_data(table_name, key_name):
+    query = f'''select * from {DATABASE_NAME}.{table_name};'''
     response = execute_statement(query, [])
     result = response['records']
 
-    return formatted_latest_timestamp_value_entry(result, RESPONSE_KEYS.buffer)
+    return Formattor().formatted_latest_timestamp_value_entry(result, key_name)
 
 def get_open_positions_data():
     query = f'''select * from {DATABASE_NAME}.{TABLE_NAMES.open_positions};'''
@@ -26,19 +24,3 @@ def get_open_positions_data():
     out = [ row[0]['stringValue'] for row in result]
 
     return out
-
-def get_share_price_data():
-    query = f'''select * from {DATABASE_NAME}.{TABLE_NAMES.share_price_db};'''
-    response = execute_statement(query, [])
-    result = response['records']
-
-    out = [ timestamp_key_row_formatter(row, RESPONSE_KEYS.price) for row in result]
-
-    return out
-
-def get_apr_data():
-    query = f'''select * from {DATABASE_NAME}.{TABLE_NAMES.share_price_db};'''
-    response = execute_statement(query, [])
-    result = response['records']
-
-    return formatted_latest_timestamp_value_entry(result, RESPONSE_KEYS.apr)
