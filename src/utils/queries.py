99a9c4ec949be8e0ad8d054ef7e8f0bd28d85ef7
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-03 19:03:34 +0530

    ft: Database class refactor

--- src/utils/queries.py
+++ src/utils/queries.py
@@ -1,25 +1,25 @@
 from configs.database import DATABASE_NAME, TABLE_NAMES
-from utils.database import execute_statement
+from utils.database import InstantiatedDB
 from utils.formatting import Formattor
 
 class Queries():
     def get_all_timestamp_value_data(self, table_name, key_name):
         query = f'''select * from {DATABASE_NAME}.{table_name};'''
-        response = execute_statement(query, [])
+        response = InstantiatedDB.execute_statement(query, [])
         result = response['records']
 
         return Formattor().formatted_all_timestamp_value_entries(result, key_name)
 
     def get_latest_timestamp_value_data(self, table_name, key_name):
         query = f'''select * from {DATABASE_NAME}.{table_name};'''
-        response = execute_statement(query, [])
+        response = InstantiatedDB.execute_statement(query, [])
         result = response['records']
 
         return Formattor().formatted_latest_timestamp_value_entry(result, key_name)
 
     def get_open_positions_data(self):
         query = f'''select * from {DATABASE_NAME}.{TABLE_NAMES.open_positions};'''
-        response = execute_statement(query, [])
+        response = InstantiatedDB.execute_statement(query, [])
         result = response['records']
 
         out = [ row[0]['stringValue'] for row in result]
