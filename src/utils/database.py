9a06d8c029cc758d779bcda9729c26721014e994
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-03 19:03:34 +0530

    ft: Database class refactor

--- src/utils/database.py
+++ src/utils/database.py
@@ -4,20 +4,24 @@ from dotenv import load_dotenv
 
 from configs.database import DATABASE_NAME
 
-load_dotenv()
+class Database():
+    def __init__(self):
+        load_dotenv()
 
-rds_client = boto3.client(
-    'rds-data', 
-    region_name='us-east-2', 
-    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-)
- 
-def execute_statement(query, params):
-    response = rds_client.execute_statement(
-        secretArn = os.getenv('DB_SECRET_ARN'),
-        resourceArn = os.getenv('DB_ARN'),
-        database = DATABASE_NAME,
-        sql = query,
-        parameters = params) 
-    return response
+        self.rds_client = boto3.client(
+            'rds-data', 
+            region_name='us-east-2', 
+            aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
+    
+    def execute_statement(self, query, params):
+        response = self.rds_client.execute_statement(
+            secretArn = os.getenv('DB_SECRET_ARN'),
+            resourceArn = os.getenv('DB_ARN'),
+            database = DATABASE_NAME,
+            sql = query,
+            parameters = params) 
+        return response
+
+InstantiatedDB = Database()
