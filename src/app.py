3d7a1250b8402c67c31dd594f7119920ed802cd0
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-03 18:59:08 +0530

    ft: used generic queries

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask_cors import CORS
 from flask import Flask, jsonify
-from utils.queries import get_apr_data, get_open_positions_data, get_share_price_data, get_historic_rewards_data, get_latest_buffer_value
+from configs.database import TABLE_NAMES
+from configs.response import RESPONSE_KEYS
+from utils.queries import Queries
 
 app = Flask(__name__)
 CORS(app)
@@ -11,28 +13,31 @@ def health_check():
 
 @app.route("/historic_rewards", methods=['GET'])
 def get_historic_rewards():
-    result = get_historic_rewards_data()
+    result = Queries().get_all_timestamp_value_data(
+        TABLE_NAMES.historic_rewards, 
+        RESPONSE_KEYS.claimed_rewards
+    )
     return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
 
 @app.route("/latest_buffer", methods=['GET'])
 def get_latest_buffer():
-    result = get_latest_buffer_value()
+    result = Queries().get_all_timestamp_value_data(TABLE_NAMES.buffer_values, RESPONSE_KEYS.buffer)
     return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
 
 @app.route("/open_timestamps", methods=['GET'])
 def get_open_positions():
-    result = get_open_positions_data()
+    result = Queries().get_open_positions_data()
     return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
 
 
 @app.route("/share_price", methods=['GET'])
 def get_share_prices():
-    result = get_share_price_data()
+    result = Queries().get_all_timestamp_value_data(TABLE_NAMES.share_price_db, RESPONSE_KEYS.price)
     return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
 
 @app.route("/apr", methods=['GET'])
 def get_apr_values():
-    result = get_apr_data()
+    result = Queries().get_latest_timestamp_value_data(TABLE_NAMES.share_price_db, RESPONSE_KEYS.apr)
     return  (jsonify(data = result), 200, {"ContentType": 'application/json'})
 
 
