1084300031ecaf91b5724d3ea839b8210d56950b
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-14 16:50:09 +0530

    ft: added batcher to account for pendingDeposit in tvl

--- src/configs/database.py
+++ src/configs/database.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass,fields
 class VaultInfo():
     name: str
     address: str
+    batcher: str
     token: str
     decimals: int
 
@@ -33,13 +34,15 @@ DATABASE_NAME = 'protected_moonshots_activity'
 VAULTS = Vaults(
     VaultInfo(
         'ethmaxi',
-        '0xAa0508FcD0352B206F558b2B817dcC1F0cc3F401', 
+        '0xAa0508FcD0352B206F558b2B817dcC1F0cc3F401',
+        '0x47c84A87A2a972769cc5DeDa28118617E3A48F8C',
         '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
         1e18
     ), 
     VaultInfo(
         'pmusdc', 
         '0x1C4ceb52ab54a35F9d03FcC156a7c57F965e081e',
+        '0x1b6BF7Ab4163f9a7C1D4eCB36299525048083B5e',
         '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
         1e6
     )
--- src/utils/on_chain.py
+++ src/utils/on_chain.py
@@ -11,9 +11,11 @@ class OnChainQueries():
     def __init__(self) -> None:
         dirname = os.path.dirname(__file__)
         vault_abi_filename = os.path.join(dirname, '../abi/vault.json')
+        batcher_abi_filename = os.path.join(dirname, '../abi/batcher.json')
         
         self.w3 = Web3(Web3.HTTPProvider(os.getenv("MAINNET_RPC")))
         self.vault_abi = json.load(open(vault_abi_filename, 'r'))
+        self.batcher_abi = json.load(open(batcher_abi_filename, 'r'))
 
     def get_tvl(self):
         balances = {}
@@ -24,6 +26,12 @@ class OnChainQueries():
                 address = vault_info.address,
                 abi = self.vault_abi
             )
+            batcher = self.w3.eth.contract(
+                address = vault_info.batcher,
+                abi = self.batcher_abi
+            )
+
+            vault_pending_deposits = batcher.functions.pendingDeposit().call()
 
             vault_total_supply = vault.functions.totalSupply().call()
             vault_share_price = (Queries().get_all_timestamp_value_data(
@@ -32,7 +40,9 @@ class OnChainQueries():
                 TABLE_NAMES.ethmaxi_share_price_db, 
                 RESPONSE_KEYS.price
             ))[-1]['price']
-            vault_tvl = round((vault_total_supply * vault_share_price) / vault_info.decimals, 2)
+            vault_tvl = round(
+                ((vault_total_supply * vault_share_price) + vault_pending_deposits) / vault_info.decimals, 2
+            )
 
             if vault_info.name in balances:
                 balances[vault_info.name] += vault_tvl
