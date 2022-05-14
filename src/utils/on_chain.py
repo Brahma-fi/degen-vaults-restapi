be36852d12375acc4c43382fe857ad1c17a1ed4b
pradeep-selva <pradeepselvaraju@gmail.com>
2022-05-14 15:58:36 +0530

    ft: on chain util

---
+++ src/utils/on_chain.py
@@ -0,0 +1,45 @@
+import os
+
+from itsdangerous import json
+from web3 import Web3
+
+from configs.database import TABLE_NAMES, VAULTS, VaultInfo
+from configs.response import RESPONSE_KEYS
+from utils.queries import Queries
+
+class OnChainQueries():
+    def __init__(self) -> None:
+        dirname = os.path.dirname(__file__)
+        vault_abi_filename = os.path.join(dirname, '../abi/vault.json')
+        
+        self.w3 = Web3(Web3.HTTPProvider(os.getenv("MAINNET_RPC")))
+        self.vault_abi = json.load(open(vault_abi_filename, 'r'))
+
+    def get_tvl(self):
+        balances = {}
+
+        for vault_name in VAULTS.get_vaults():
+            vault_info: VaultInfo = getattr(VAULTS,vault_name)
+            vault = self.w3.eth.contract(
+                address = vault_info.address,
+                abi = self.vault_abi
+            )
+
+            vault_total_supply = vault.functions.totalSupply()
+            vault_share_price = (Queries().get_all_timestamp_value_data(
+                TABLE_NAMES.share_price_db 
+                if vault_name == VAULTS.pmusdc.name else 
+                TABLE_NAMES.ethmaxi_share_price_db, 
+                RESPONSE_KEYS.price
+            ))['data'][-1]
+            vault_tvl = vault_total_supply * vault_share_price
+
+            if vault_info.name in balances:
+                balances[vault_info.name] += vault_tvl
+            else:
+                balances[vault_info.name] = vault_tvl
+
+        return balances
+
+
+    
